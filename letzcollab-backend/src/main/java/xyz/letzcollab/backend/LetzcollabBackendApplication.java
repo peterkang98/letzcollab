ba5726bce380b5e000b49f34611287fdbf9e900e
강동현 <peterkang98@gmail.com>
2026-02-10 19:18:39 +0900

    첫 커밋

---
+++ letzcollab-backend/src/main/java/xyz/letzcollab/backend/LetzcollabBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.letzcollab.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetzcollabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetzcollabBackendApplication.class, args);
+	}
+
+}
---
+++ letzcollab-backend/src/test/java/xyz/letzcollab/backend/LetzcollabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.letzcollab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetzcollabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
