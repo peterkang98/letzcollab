f2124ae877e9828a2ba7e6c20f07971006bff5d0
강동현 <peterkang98@gmail.com>
2026-02-12 06:19:27 +0900

    import문 정리

--- letzcollab-backend/src/main/java/xyz/letzcollab/backend/global/exception/GlobalExceptionHandler.java
+++ letzcollab-backend/src/main/java/xyz/letzcollab/backend/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package xyz.letzcollab.backend.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
