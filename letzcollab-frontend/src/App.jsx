9369cfdb89625888d1610a56142aaf37d136317d
강동현 <peterkang98@gmail.com>
2026-02-19 23:04:48 +0900

    App.jsx 작성 완료. 리액트 쿼리 컨텍스트 제공 + 라우팅 설정

--- letzcollab-frontend/src/App.jsx
+++ letzcollab-frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import Login from "./pages/Login";
+import AuthLayout from "./layouts/AuthLayout.jsx";
+import PublicRoute from "./routes/PublicRoute.jsx";
+import Signup from "./pages/Signup.jsx";
+import VerifyEmail from "./pages/VerifyEmail.jsx";
+import RequestPasswordReset from "./pages/RequestPasswordReset.jsx";
+import ResetPassword from "./pages/ResetPassword.jsx";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            {/* 인증이 필요한 페이지들의 레이아웃과 라우팅 설정 */}
+            <Route element={<PrivateRoute/>}>
+              <Route element={<MainLayout/>}>
+                <Route path="/" element={<></>}/>
+              </Route>
+            </Route>
+
+            {/* 인증이 필요 없는 페이지들의 레이아웃과 라우팅 설정 (로그인한 사람들은 접속 불가) */}
+            <Route element={<PublicRoute/>}>
+              <Route element={<AuthLayout/>}>
+                <Route path="/auth/login" element={<Login/>}/>
+                <Route path="/auth/signup" element={<Signup/>}/>
+                <Route path="/auth/verify-email" element={<VerifyEmail/>}/>
+                <Route path="/auth/password/reset-request" element={<RequestPasswordReset/>}/>
+                <Route path="/auth/password/reset" element={<ResetPassword/>}/>
+              </Route>
+            </Route>
+
+            {/* 잘못된 경로 접속 시 홈으로 리다이렉트 */}
+            <Route path="*" element={<Navigate to="/"/>}/>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
