ea3e22b4b99c248a12f8dd67b5fefddd05b5410a
강동현 <peterkang98@gmail.com>
2026-02-21 06:44:47 +0900

    JWT를 제시하지 않고 인증이 필요한 경로를 접근할때 403 대신 401 응답을 내려주도록 변경.
    AuthenticationEntryPoint 구현체를 만들어서 필터 체인에 추가

--- letzcollab-backend/src/main/java/xyz/letzcollab/backend/global/security/SecurityConfig.java
+++ letzcollab-backend/src/main/java/xyz/letzcollab/backend/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package xyz.letzcollab.backend.global.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -14,10 +15,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import xyz.letzcollab.backend.global.security.jwt.JwtAuthenticationEntryPoint;
 import xyz.letzcollab.backend.global.security.jwt.JwtAuthenticationFilter;
 
 @Configuration
@@ -35,7 +38,8 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(
 			HttpSecurity http,
 			JwtAuthenticationFilter jwtAuthenticationFilter,
-			CorsConfigurationSource corsConfigurationSource
+			CorsConfigurationSource corsConfigurationSource,
+			JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint
 	) throws Exception {
 		http
 			.cors(cors -> cors.configurationSource(corsConfigurationSource))
@@ -47,6 +51,7 @@ public class SecurityConfig {
 				auth.anyRequest().authenticated();
 			})
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.exceptionHandling(conf -> conf.authenticationEntryPoint(jwtAuthenticationEntryPoint))
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
