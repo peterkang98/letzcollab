eb0ad08de367c9db37dfc9d52233641d9b10fe63
강동현 <peterkang98@gmail.com>
2026-02-27 22:33:09 +0900

    UUID 검증 어노테이션을 NotNull로 수정

--- letzcollab-backend/src/main/java/xyz/letzcollab/backend/dto/auth/PasswordResetRequest.java
+++ letzcollab-backend/src/main/java/xyz/letzcollab/backend/dto/auth/PasswordResetRequest.java
@@ -1,12 +1,13 @@
 package xyz.letzcollab.backend.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 import java.util.UUID;
 
 public record PasswordResetRequest(
-	@NotBlank(message = "검증 토큰은 필수입니다.")
+	@NotNull(message = "검증 토큰은 필수입니다.")
 	UUID token,
 
 	@NotBlank(message = "비밀번호는 필수입니다")
--- letzcollab-backend/src/main/java/xyz/letzcollab/backend/dto/auth/ResendEmailVerificationRequest.java
+++ letzcollab-backend/src/main/java/xyz/letzcollab/backend/dto/auth/ResendEmailVerificationRequest.java
@@ -1,12 +1,11 @@
 package xyz.letzcollab.backend.dto.auth;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record ResendEmailVerificationRequest(
-	@NotBlank(message = "검증 토큰은 필수입니다.")
+	@NotNull(message = "검증 토큰은 필수입니다.")
 	UUID expiredToken
 ) {
 }
