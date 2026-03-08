8d4dc41e3d1677c789b8667f5f937eacfd4ca433
강동현 <peterkang98@gmail.com>
2026-03-08 22:35:43 +0900

    스프링의 Page 타입의 데이터를 PagedModel이라는 표준화된 DTO 형식으로 변환해서 응답으로 내보내는 설정 추가

---
+++ letzcollab-backend/src/main/java/xyz/letzcollab/backend/global/config/WebConfig.java
@@ -0,0 +1,10 @@
+package xyz.letzcollab.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig implements WebMvcConfigurer {
+}
