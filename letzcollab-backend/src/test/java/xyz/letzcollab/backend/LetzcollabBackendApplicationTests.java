07d77e654c1179d1d970661a362c825502dddf75
강동현 <peterkang98@gmail.com>
2026-03-13 04:58:12 +0900

    .

--- letzcollab-backend/src/test/java/xyz/letzcollab/backend/LetzcollabBackendApplicationTests.java
+++ letzcollab-backend/src/test/java/xyz/letzcollab/backend/LetzcollabBackendApplicationTests.java
@@ -2,8 +2,10 @@ package xyz.letzcollab.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LetzcollabBackendApplicationTests {
 
 	@Test
