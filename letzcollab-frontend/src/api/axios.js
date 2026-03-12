e1a95dbb1e2c93d6071e1beb7e2651f5f4a4e246
강동현 <peterkang98@gmail.com>
2026-03-12 12:18:06 +0900

    axios의 백엔드 요청 경로를 로컬/prod 환경에 따라 동적으로 변경하도록 수정

--- letzcollab-frontend/src/api/axios.js
+++ letzcollab-frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 매번 쿠키를 Cookie 헤더에 실어서 서버에 요청
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: import.meta.env.PROD ? '/api/v1' : 'http://localhost:8080/api/v1',
   withCredentials: true
 })
 
